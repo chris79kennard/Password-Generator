48b20ae16a3d1378abe816064a9364072c3e4828
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-09-23 18:56:18 -0500

    set up forLoop and return newPassword  to get rid of not definded issue on webpage display.

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,6 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-  var Length = document.querySelector("#Length").value;
-  console.log(Length);
-  // start adding if statements to see if it has an effect on the the display text
-  var Uppercase = document.querySelector("#Uppercase").checked;
-  console.log(Uppercase);
-  var Lowercase = document.querySelector("#Lowercase").checked;
-  console.log(Lowercase);
-  var Numbers = document.querySelector("#Numbers").checked;
-  console.log(Numbers);
-  var Special = document.querySelector("#Special").checked;
-  console.log(Special);
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  console.log(passwordText.value);
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Create arrays of characters
+var newRandomChar = "";
+var newPassword = "";
 var passwordChars = [];
 var numberChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var upperChars = [
@@ -89,10 +61,64 @@ var lowerChars = [
 ];
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
-// ********************************************************
-// var generatePassword = function () {
-//   return password;
-// }; **NOT SURE YET**
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var Length = document.querySelector("#Length").value;
+  console.log(Length);
+
+  var Uppercase = document.querySelector("#Uppercase").checked;
+  console.log(Uppercase);
+
+  var Lowercase = document.querySelector("#Lowercase").checked;
+  console.log(Lowercase);
+
+  var Numbers = document.querySelector("#Numbers").checked;
+  console.log(Numbers);
+
+  var Special = document.querySelector("#Special").checked;
+  console.log(Special);
+  if (Uppercase) {
+    passwordChars = passwordChars.concat(upperChars);
+  }
+  console.log(passwordChars);
+
+  if (Lowercase) {
+    passwordChars = passwordChars.concat(lowerChars);
+  }
+  console.log(passwordChars);
+
+  if (Numbers) {
+    passwordChars = passwordChars.concat(numberChars);
+  }
+  console.log(passwordChars);
+
+  if (Special) {
+    passwordChars = passwordChars.concat(specialChars);
+  }
+  console.log(passwordChars);
+
+  for (i = 0; i < Length; i++) {
+    var index = Math.floor(Math.random() * passwordChars.length);
+    newRandomChar = passwordChars[index];
+    newPassword = newPassword.concat(newRandomChar);
+  }
+  return newPassword;
+}
+
+// Write password to the #password input
+// start adding if statements to see if it has an effect on the the display text
+function writePassword() {
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  console.log(passwordText.value);
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
