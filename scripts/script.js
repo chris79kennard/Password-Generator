cdd0ee1c0271e71f530e8fad3d2b3df267ea314c
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-09-22 22:23:22 -0500

    JavaScript  reads variables from HTML.

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  var Length = document.querySelector("#Length").value;
+  console.log(Length);
+  var Uppercase = document.querySelector("#Uppercase").checked;
+  console.log(Uppercase);
+  var Lowercase = document.querySelector("#Lowercase").checked;
+  console.log(Lowercase);
+  var Numbers = document.querySelector("#Numbers").checked;
+  console.log(Numbers);
+  var Special = document.querySelector("#Special").checked;
+  console.log(Special);
 }
 
 // Add event listener to generate button
