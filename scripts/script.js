d0daf428b645900c960a31d9513cd898ac03d325
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-09-23 17:46:17 -0500

    created arrays of characters needed to produce a password

--- scripts/script.js
+++ scripts/script.js
@@ -25,12 +25,74 @@ function writePassword() {
   console.log(passwordText.value);
 }
 
-// var generatePassword = function () {
-//   return password;
-// };
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create arrays of characters
+var passwordChars = [];
+var numberChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var upperChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+// ********************************************************
+// var generatePassword = function () {
+//   return password;
+// }; **NOT SURE YET**
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
